26e68add43dcf0d2bac9955481846682dd2408b9
ahmed6394 <96921439+ahmed6394@users.noreply.github.com>
2025-04-03 11:02:47 +0000

    contact hpp done

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+
+void Contact::set_firstname(string firstname)
+{
+    this->firstname = firstname;
+}
+void Contact::set_lastname(string lastname)
+{
+    this->lastname = lastname;
+}
+void Contact::set_nickname(string nickname)
+{
+    this->nickname = nickname;
+}
+void Contact::set_secrate(string secrate)
+{
+    this->secrate = secrate;
+}
+void Contact::set_number(string number)
+{
+    this->number = number;
+}
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -10,3 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+using namespace std;
+
+class Contact
+{
+private:
+    string firstname;
+    string lastname;
+    string nickname;
+    string secrate;
+    string number;
+public:
+    void set_firstname(string firstname);
+    void set_lastname(string lastname);
+    void set_nickname(string nickname);
+    void set_secrate(string secrate);
+    void set_number(string number);
+    string get_firstname();
+    string get_lastname();
+    string get_nickname();
+    string get_secrate();
+    string get_number();
+
+};
+
